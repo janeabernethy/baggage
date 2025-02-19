1cbda00051c14ec9c17ad977056141e6ef20a8d2
Jane <janeainmotion@gmail.com>
2025-02-20 09:30:59 +1100

    Sponsor links

--- src/app/Crew.tsx
+++ src/app/Crew.tsx
@@ -236,15 +236,15 @@ const SpecialThanks = React.memo(({ disableAnimations, isMobile }: { disableAnim
             <div className={styles.crewHeader} style={{paddingBottom: 25}}>Supported By</div>
             <div className={styles.supportedByContainer}>
             <div className={isMobile ? styles.supportedByMobile : styles.supportedBy}>
-               <SupportImg imgName="/supportedBy/aard.jpg" alt="Aardman" />
-               <SupportImg imgName="/supportedBy/IPCF.jpg" alt="Ian Potter Culteral Trust" />
+               <SupportImg imgName="/supportedBy/aard.jpg" alt="Aardman" link="https://academy.aardman.com"/>
+               <SupportImg imgName="/supportedBy/IPCF.jpg" alt="Ian Potter Culteral Trust" link="https://www.ianpotter.org.au/"/>
 
-               <SupportImg imgName="/supportedBy/vicscreen.jpg" alt="Vic Screen" />
+               <SupportImg imgName="/supportedBy/vicscreen.jpg" alt="Vic Screen" link="https://vicscreen.vic.gov.au/news/from-melbourne-to-aardman-the-stop-motion-journey-of-lucy-davidson" />
 
 
       
-              <SupportImg imgName="/supportedBy/HAB.jpg" alt="Hunt and Brew" />
-               <SupportImg imgName="/supportedBy/SA.jpg" alt="Screen Australia" />
+              <SupportImg imgName="/supportedBy/HAB.jpg" alt="Hunt and Brew" link="https://huntandbrew.com/" />
+               <SupportImg imgName="/supportedBy/SA.jpg" alt="Screen Australia" link="https://www.screenaustralia.gov.au/the-screen-guide/t/baggage-2024/43006/" />
 
 
             </div>
@@ -254,8 +254,8 @@ const SpecialThanks = React.memo(({ disableAnimations, isMobile }: { disableAnim
     )
 });
 
-const SupportImg = React.memo(({imgName, alt} : {imgName: string, alt: string}) => {
-    return ( <div className={styles.supportImgContainer }>
+const SupportImg = React.memo(({imgName, alt, link} : {imgName: string, alt: string, link: string}) => {
+    return ( <div className={styles.supportImgContainer } onClick={() => {window.open(link)}}>
         <img className={styles.supportImg} src={imgName} alt={alt} />
     </div>)
 })
