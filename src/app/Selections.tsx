da498a82a7b8d20b783597f2ddeb777aaa5b82e0
Jane <janeainmotion@gmail.com>
2025-02-20 09:39:14 +1100

    Selection links

--- src/app/Selections.tsx
+++ src/app/Selections.tsx
@@ -65,25 +65,25 @@ export const Selections = React.memo(({ disableAnimations, isMobile, selectionRe
         </div>
       </div>}
       <div ref={selectionImgsRef} className={isMobile ? styles.selectionContainerMobile : styles.selectionContainer}>
-        <div onClick={() => window.open("")} className={styles.selectionImageContainer}>
+        <div onClick={() => window.open("https://schedule.sxsw.com/2025/directors/2206193")} className={styles.selectionImageContainer}>
           <img className={styles.selectionImage} src={isMobile ? "/laurels/mobile/sxsw.jpeg"  : "/laurels/sxsw.jpeg" }/>
         </div>
-        <div onClick={() => window.open("")} className={styles.selectionImageContainer}>
+        <div onClick={() => window.open("https://liaf.org.uk/schedule/liaf-2024-international-competition-programme-6-stop-motion-panorama-15?wcs_timestamp=1732827600")} className={styles.selectionImageContainer}>
           <img className={styles.selectionImage} src={isMobile ? "/laurels/mobile/liaf.jpg"  :"/laurels/liaf.jpeg"} />
         </div>
-        <div onClick={() => window.open("")} className={styles.selectionImageContainer}>
+        <div onClick={() => window.open("https://flickerfest.com.au/programme/best-of-australian-shorts-1-2025/")} className={styles.selectionImageContainer}>
           <img className={styles.selectionImage} src={isMobile ? "/laurels/mobile/flickerfest.jpg"  :"/laurels/flickerfest.jpeg" }/>
         </div>
-        <div onClick={() => window.open("")} className={styles.selectionImageContainer}>
+        <div onClick={() => window.open("https://www.instagram.com/p/DFW6v-_y5ow/")} className={styles.selectionImageContainer}>
           <img className={styles.selectionImage} src={isMobile ? "/laurels/mobile/peninsula.jpg" : "/laurels/peninsula.jpeg" }/>
         </div>
-        <div onClick={() => window.open("")} className={styles.selectionImageContainer}>
+        <div onClick={() => window.open("https://animationdingle.com/home-2/")} className={styles.selectionImageContainer}>
           <img className={styles.selectionImage} src={isMobile ? "/laurels/mobile/dingle.jpg" : "/laurels/dingle.jpg"} />
         </div>
-        <div onClick={() => window.open("")} className={styles.selectionImageContainer}>
+        <div onClick={() => window.open("https://www.clevelandfilm.org/")} className={styles.selectionImageContainer}>
           <img className={styles.selectionImage} src={isMobile ? "/laurels/mobile/cleveland.jpg" : "/laurels/cleveland.jpeg" }/>
         </div>
-        <div onClick={() => window.open("")} className={styles.selectionImageContainer}>
+        <div onClick={() => window.open("https://www.brightoninternationalanimationfestival.com/")} className={styles.selectionImageContainer}>
           <img className={styles.selectionImage} src={isMobile ? "/laurels/mobile/biaf.jpg": "/laurels/biaf.jpeg" }/>
         </div>
       </div>
