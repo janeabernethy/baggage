6b4a977cf136257b8516d7e9114e49dc3b8c807e
Jane <janeainmotion@gmail.com>
2025-02-20 12:15:06 +1100

    social mobile

--- src/app/Crew.tsx
+++ src/app/Crew.tsx
@@ -56,7 +56,13 @@ export const Crew = React.memo(({ disableAnimations, crewRef, height, isMobile }
         <CoreCrew disableAnimations={disableAnimations} isMobile={isMobile} />
         <Cast disableAnimations={disableAnimations} isMobile={isMobile} />
         <CoreCrew2 disableAnimations={disableAnimations} isMobile={isMobile} />
-        <SpecialThanks disableAnimations={disableAnimations} isMobile={isMobile} />
+        <SupportedBy disableAnimations={disableAnimations} isMobile={isMobile} />
+        {isMobile &&
+            <div className={styles.crewContent3}>
+
+                <CrewPic disableAnimations={disableAnimations} src={"credits/3.jpg"} isMobile={isMobile} />
+            </div>
+        }
     </div>
 });
 
@@ -209,53 +215,37 @@ const Tutors = React.memo(({ disableAnimations, isMobile }: { disableAnimations:
     )
 });
 
-const TechSupport = React.memo(({ disableAnimations, isMobile }: { disableAnimations: boolean, isMobile: boolean }) => {
-    const crewSections: CrewSection[] = [
-        { header: "Studio Manager", names: ["Nat Sale"] },
-        { header: "Senior Studio Support Technician", names: ["Adam Cook"] },
-        { header: "Studio System Support Assistant", names: ["Adam Hanney", "Ollie Orwell"] },
-        { header: "Studio Systems Support Engineer", names: ["Will Marshall"] },
-        { header: "Senior Physical & Post Production Engineer", names: ["Paul Reeves"] },
-        { header: "Digital Production Technology Manager", names: ["Toby Chilcot"] },
-    ];
+
+
+const SupportedBy = React.memo(({ disableAnimations, isMobile }: { disableAnimations: boolean, isMobile: boolean }) => {
+
+
 
     return (
-        <div className={styles.crewContent3}>
-            <CrewSection disableAnimations={disableAnimations} isMobile={isMobile} mobileImg={"/guts/rich.jpg"} sections={crewSections} />
-
-        </div>
-    )
-});
-
-const SpecialThanks = React.memo(({ disableAnimations, isMobile }: { disableAnimations: boolean, isMobile: boolean }) => {
-
-   
-
-    return (
-        <div className={styles.crewContent3} style={{padding: "50px 0px"}}>
-            <div className={styles.crewHeader} style={{paddingBottom: 25}}>Supported By</div>
+        <div className={styles.crewContent3} style={{ padding: "50px 0px" }}>
+            <div className={styles.crewHeader} style={{ paddingBottom: 25 }}>Supported By</div>
             <div className={styles.supportedByContainer}>
-            <div className={isMobile ? styles.supportedByMobile : styles.supportedBy}>
-               <SupportImg imgName="/supportedBy/aard.jpg" alt="Aardman" link="https://academy.aardman.com"/>
-               <SupportImg imgName="/supportedBy/IPCF.jpg" alt="Ian Potter Culteral Trust" link="https://www.ianpotter.org.au/"/>
+                <div className={isMobile ? styles.supportedByMobile : styles.supportedBy}>
+                    <SupportImg imgName="/supportedBy/aard.jpg" alt="Aardman" link="https://academy.aardman.com" />
+                    <SupportImg imgName="/supportedBy/IPCF.jpg" alt="Ian Potter Culteral Trust" link="https://www.ianpotter.org.au/" />
 
-               <SupportImg imgName="/supportedBy/vicscreen.jpg" alt="Vic Screen" link="https://vicscreen.vic.gov.au/news/from-melbourne-to-aardman-the-stop-motion-journey-of-lucy-davidson" />
-
-
-      
-              <SupportImg imgName="/supportedBy/HAB.jpg" alt="Hunt and Brew" link="https://huntandbrew.com/" />
-               <SupportImg imgName="/supportedBy/SA.jpg" alt="Screen Australia" link="https://www.screenaustralia.gov.au/the-screen-guide/t/baggage-2024/43006/" />
+                    <SupportImg imgName="/supportedBy/vicscreen.jpg" alt="Vic Screen" link="https://vicscreen.vic.gov.au/news/from-melbourne-to-aardman-the-stop-motion-journey-of-lucy-davidson" />
 
 
-            </div>
+
+                    <SupportImg imgName="/supportedBy/HAB.jpg" alt="Hunt and Brew" link="https://huntandbrew.com/" />
+                    <SupportImg imgName="/supportedBy/SA.jpg" alt="Screen Australia" link="https://www.screenaustralia.gov.au/the-screen-guide/t/baggage-2024/43006/" />
+
+
+                </div>
             </div>
 
         </div>
     )
 });
 
-const SupportImg = React.memo(({imgName, alt, link} : {imgName: string, alt: string, link: string}) => {
-    return ( <div className={styles.supportImgContainer } onClick={() => {window.open(link)}}>
+const SupportImg = React.memo(({ imgName, alt, link }: { imgName: string, alt: string, link: string }) => {
+    return (<div className={styles.supportImgContainer} onClick={() => { window.open(link) }}>
         <img className={styles.supportImg} src={imgName} alt={alt} />
     </div>)
 })
