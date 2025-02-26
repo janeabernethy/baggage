af68214a48a0a8893c86e4e0cad8ff9c5fdaa2b5
Jane <janeainmotion@gmail.com>
2025-02-26 17:52:44 +1100

    Rearrange landing

--- src/app/Landing.tsx
+++ src/app/Landing.tsx
@@ -2,19 +2,24 @@ import React from 'react';
 import styles from "./baggage.module.css";
 
 
-export const Landing = ({ landingRef, disableAnimations }: { landingRef: React.RefObject<HTMLDivElement|null>, disableAnimations: boolean}) => {
+export const Landing = ({ landingRef, disableAnimations }: { landingRef: React.RefObject<HTMLDivElement | null>, disableAnimations: boolean }) => {
   return <div ref={landingRef} className={styles.landing}>
     <div className={styles.videoContainer}>
       {disableAnimations &&
-        <img style={{width: "100%", "borderRadius": 20}}src="titlethumb.avif" alt="baggage poster" />
-     }
-      {!disableAnimations && 
-      <video muted playsInline className={styles.landingVideo} loop autoPlay>
-        <source src='BAGGAGE-trailer-footage.mp4' type='video/mp4' />
-        Your browser does not support the video tag.
+        <img style={{ width: "100%", "borderRadius": 20 }} src="titlethumb.avif" alt="baggage poster" />
+      }
+      {!disableAnimations &&
+        <video muted playsInline className={styles.landingVideo} loop autoPlay>
+          <source src='BAGGAGE-trailer-footage.mp4' type='video/mp4' />
+          Your browser does not support the video tag.
 
-      </video>
-    } 
+        </video>
+      }
     </div>
+
+    <div className={styles.aboutDesc}>
+      Three girlfriends check in their baggage at the airport, but one is carrying a little more than the others. As they travel along the conveyor belt to security, can she hide what’s inside?
+    </div>
+
   </div>
 }
